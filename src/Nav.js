d0194211ae4e8ddb3d5cbed49292aecb04d8d22d
Piyush Jain <piyush.pj936@gmail.com>
2022-06-19 13:33:03 +0530

    Complete Code

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import user_profile from "./images/user_profile.png";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+
+      <img className="nav__avatar" src={user_profile} alt="Avatar Logo" />
+    </div>
+  );
+}
+
+export default Nav;
